e84f8a5a7640e77906f84c90fe2713e8ad07f511
abigailnm111 <60757439+abigailnm111@users.noreply.github.com>
2021-07-10 22:48:23 -0700

    further clean data

--- mccall_scraper.py
+++ mccall_scraper.py
@@ -8,19 +8,78 @@ Created on Tue Jun 29 10:26:25 2021
 import scrapy
 from scrapy.item import Item, Field
 from scrapy.loader import ItemLoader
-from itemloaders.processors import TakeFirst, MapCompose
+from itemloaders.processors import TakeFirst, MapCompose, Compose
 
 
 import re
 
-def line_filter_out(x):
+def line_clean(x):
    
      l1= re.sub(r"\r\n",'', x)
      line=re.sub(r"\n", '', l1)
-        
+     if line==' ':
+         line== None
+
+     
      return line
 
+def audiance_determination(x):
+    new=[]
+    if re.findall('Misses|Men|Women',x):
+        new.append('Adult')
+    if re.findall('Boy|Girl|Children|Infant|Toddler',x):
+        new.append('Children')
+    if re.findall('Petite',x):
+        new.append("Petite")
+    if new==[]:
+        new.append('Unknown')
 
+    return new
+
+
+
+def garment_type_determination(x):
+    new=[]
+    if re.findall('Top|Blouse|Tunic|Shirt',x):
+        new.append("Top")
+    if re.findall('Dress',x):
+        new.append("Dress")
+    if re.findall("Shorts",x):
+        new.append("Shorts")
+    if re.findall("Pants|Legging",x):
+        new.append("Pants")
+    if re.findall("Skirt",x):
+        new.append("Skirt")
+    if re.findall("Vest", x):
+        new.append("Vest")
+    if re.findall("Cardigan|Sweater",x):
+        new.append("Cardigan/Sweater")
+    if re.findall("Jacket|Coat|Hoodie",x):
+        new.append("Jacket/Coat")
+    if re.findall("Jumpsuit|Romper|Overalls",x):
+        new.append("Jumpsuit/Romper/Overalls")
+    return new
+        
+        
+def fabric_clean(x):
+
+    line= re.sub("[*].+|FABRICS:",'', x)
+    if line== ' ':
+        line= None
+    return line
+        
+
+# def notions_clean(x):
+#     line=re.sub("NOTIONS:",'', x)
+    
+    
+# def sizes_clean(x):
+#     line=re.sub("Size\sCombinations",'', x)
+    
+# def clean_lists(x):
+    
+    
+    
 
 # class PatternSpiderPipline(object):
 #       def process_item(self, item, spider):
@@ -30,20 +89,25 @@ class Pattern(Item):
     name= Field(
         output_processor=TakeFirst(),
         )
-    alt_desc= Field(
-        input_processor= MapCompose(line_filter_out),
+    audiance=Field(
+        input_processor= MapCompose(line_clean, audiance_determination),
+        
+        )
+    garment_type= Field(
+        input_processor= MapCompose(line_clean, garment_type_determination),
                     )
     description=Field(
-        input_processor= MapCompose(line_filter_out),
+        input_processor= MapCompose(line_clean),
         )
     fabric= Field(
-        input_processor= MapCompose(line_filter_out),
+        input_processor= MapCompose(line_clean, fabric_clean),
+        #output_processor=Compose(lambda x: x[0], str.split(",") )
         )
     notions= Field(
-        input_processor= MapCompose(line_filter_out),
+        input_processor= MapCompose(line_clean),
                    )
     sizes= Field(
-        input_processor= MapCompose(line_filter_out),
+        input_processor= MapCompose(line_clean),
                  )
     
 
@@ -71,7 +135,8 @@ class PatternsSpider(scrapy.Spider):
             
             p=ItemLoader(item=Pattern(), response=response)
             p.add_css('name',NAME_SELECTOR)
-            p.add_css('alt_desc', ALT_DESCRIPTION_SELECTOR)
+            p.add_css('audiance', ALT_DESCRIPTION_SELECTOR)
+            p.add_css('garment_type', ALT_DESCRIPTION_SELECTOR)
             p.add_xpath('description', DESCRIPTION_SELECTOR)
             p.add_xpath('fabric', FABRIC_SELECTOR)
             p.add_xpath('notions', NOTIONS_SELECTOR)
@@ -86,4 +151,3 @@ class PatternsSpider(scrapy.Spider):
 
 #div class="form-field option--size"#product-listing-container > 
 
-#m7247
